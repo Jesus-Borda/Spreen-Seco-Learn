c66090ca217ecd308c39b3c136e866a09b9add56
Jesus Borda <jesusdeveloper2314@gmail.com>
2026-07-04 20:35:30 -0500

    guardado de emergencia

--- src/main/java/com/curso/spring_security_course/entity/User.java
+++ src/main/java/com/curso/spring_security_course/entity/User.java
@@ -2,10 +2,17 @@ package com.curso.spring_security_course.entity;
 
 import com.curso.spring_security_course.util.Role;
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "user")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -39,6 +46,8 @@ public class User {
     public String getUsername() {
         return username;
     }
+
+
 
     public void setUsername(String username) {
         this.username = username;
@@ -77,5 +86,34 @@ public class User {
                 ", password='" + password + '\'' +
                 ", role=" + role +
                 '}';
+    }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities =role.getPermissions().stream()
+                .map(permissionEnum -> new SimpleGrantedAuthority(permissionEnum.name()))
+                .collect(Collectors.toList());
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + role.name()));
+        return authorities;
     }
 }
--- src/main/java/com/curso/spring_security_course/repository/UserRepository.java
+++ src/main/java/com/curso/spring_security_course/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.curso.spring_security_course.repository;
 import com.curso.spring_security_course.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User , Long> {
+    Optional<User> findByUsername(String username);
 }
